c09fff2d667284076a998cf495a9363db2f4841b
TaiMeade <taitaishi123@gmail.com>
2026-04-26 12:22:51 -0400

    update reviews to accept time

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,20 +1,20 @@
 import moment from 'moment'
 
 // Add each review as an object in this array.
-// Fields: author (string), rating (1-5), text (string), date (YYYY-MM-DD string), profilePhotoUrl (string or null)
+// Fields: author (string), rating (1-5), text (string), date (YYYY-MM-DD HH:mm:ss string), profilePhotoUrl (string or null)
 const reviewData = [
   {
     author: 'Dylan Darnell',
     rating: 5,
     text: 'Great service, punctual and communicative. Highly recommend.',
-    date: '2026-04-24',
+    date: '2026-04-24 12:00:00',
     profilePhotoUrl: null,
   },
   {
     author: 'Ashley Bailey',
     rating: 5,
     text: 'Fantastic, quick and efficient service!!! Sam is fantastic! Very affordable!',
-    date: '2026-04-25',
+    date: '2026-04-25 18:15:00',
     profilePhotoUrl: null,
   }
 ]
