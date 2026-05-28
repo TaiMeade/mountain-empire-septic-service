48fe1ac91a4648f0055bc18e1f37b530a650c2ad
TaiMeade <taitaishi123@gmail.com>
2026-05-28 08:24:32 -0400

    update Google reviews

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -30,6 +30,27 @@ const reviewData = [
     text: "They went above and beyond truly happy with the work I highly recommend this company!\nThank you!\nJohn Carroll",
     date: '2026-05-01 09:30:00',
     profilePhotoUrl: null,
+  },
+  {
+    author: 'Renee Knight',
+    rating: 5,
+    text: "Professional, informative, easy communication! Did a great job! Call Sam for all your septic service needs!",
+    date: '2026-05-21 12:00:00',
+    profilePhotoUrl: null,
+  },
+  {
+    author: 'Samantha Reed',
+    rating: 5,
+    text: 'Very quick to respond, and excellent service! These guys were very professional and efficient!',
+    date: '2026-05-26 12:00:00',
+    profilePhotoUrl: null,
+  },
+  {
+    author: 'Greg Troxwell',
+    rating: 5,
+    text: 'Sam and crew were totally professional, did what they said they would do, were priced competitively and were here to pump the day after I called. Highly recommend this company.',
+    date: '2026-05-27 17:00:00',
+    profilePhotoUrl: null,
   }
 ]
 
